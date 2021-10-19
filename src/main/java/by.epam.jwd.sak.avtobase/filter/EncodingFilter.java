54ab1f393680857ed8748192a49dab834071cbab
Denis Sak <denissak9@gmail.com>
2021-10-19 18:33:36 +0300

    fix filter enc

--- src/main/java/by.epam.jwd.sak.avtobase/filter/EncodingFilter.java
+++ src/main/java/by.epam.jwd.sak.avtobase/filter/EncodingFilter.java
@@ -1,6 +1,4 @@
-/*
 package by.epam.jwd.sak.avtobase.filter;
-
 
         import javax.servlet.*;
         import javax.servlet.annotation.WebFilter;
@@ -12,10 +10,14 @@ public class EncodingFilter implements Filter {
     public static final String CHAR = "UTF-8";
 
     @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         servletRequest.setCharacterEncoding(CHAR);
         servletResponse.setCharacterEncoding(CHAR);
         filterChain.doFilter(servletRequest,servletResponse);
     }
 }
-*/
